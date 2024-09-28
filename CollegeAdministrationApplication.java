f863944684cc14ad89e155c1512185a977b2e8b6
Sanskar Sharma <sanskarsharma2608@gmail.com>
2024-09-28 13:55:32 +0530

    Create CollegeAdministrationApplication.java

---
+++ CollegeAdministrationApplication.java
@@ -0,0 +1,13 @@
+package com.sanskar.collegeAdmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeAdministrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeAdministrationApplication.class, args);
+	}
+
+}
